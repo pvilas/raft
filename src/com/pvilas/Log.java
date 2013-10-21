c1ebfdec6fc2cc0668059e69d6bd2a0d21b5a117
Pere Vilas <pvilas@gmail.com>
2013-10-21 20:27:25 +0200

    mes eleccio de lider

--- src/com/pvilas/Log.java
+++ src/com/pvilas/Log.java
@@ -16,41 +16,58 @@ public class Log {
     * implements the Raft log
      */
     private Debug logger;
+    private int commitIndex;
 
     private Vector entries; // entries in the log
 
     public Log(int numServer) {
-        this.logger= new com.pvilas.Debug("LOG"+numServer, Debug.DEBUG, System.out);
+        this.logger = new com.pvilas.Debug("LOG" + numServer, Debug.DEBUG, System.out);
         this.entries = new Vector();
         this.logger.debug("Log created");
+        this.commitIndex=-1;
     }
 
     // appends to the log and returns new index or -1 if
     // the entry is rejected
-    public int append(long term,
+    public boolean append(int term,
                       Object parameters,
-                      long prevLogTerm,
+                      int prevLogTerm,
                       int prevLogIndex
     ) {
 
         // 1st -- perform entry rejection algorithm
         // --------------------------------
-        if (this.lastLogIndex() == prevLogIndex && this.lastLogTerm() == prevLogTerm ) {
-            logger.debug("Inserting a new log entry");
+        // term of the entry number prevLogIndex
+        boolean reply = false;
+        int lTerm = this.getTermAtIndex(prevLogIndex);
 
-            // try to extract the command
-            String command=((JsonObject)parameters).get("command").toString();
-            LogEntry ne = new LogEntry(term, command, parameters);
-
-            this.entries.add(ne);
-
-            // return the last index
-            return this.entries.size();
-
-        }   else {
-            logger.warning("Received an outsequenced entry!");
-            return -1;
+        // reply false if index does not exist or entry rejected
+        if (lTerm != -1 ||  // index does exist
+                lTerm == prevLogTerm // terms does match
+                ) {
+            reply = true;
         }
+
+        // same index but different terms
+        if (lTerm != prevLogTerm) {
+            logger.debug("Removing log entries from "+prevLogIndex+" to the end");
+            // delete existing entry an all that follow it
+            int s = this.entries.size();
+            for (int a = prevLogIndex; a < s; a++) {
+                this.entries.remove(prevLogIndex);
+            }
+        }
+
+        logger.debug("Inserting a new log entry");
+
+        // try to extract the command
+        String command = ((JsonObject) parameters).get("command").toString();
+        LogEntry ne = new LogEntry(term, command, parameters);
+
+        this.entries.add(ne);
+
+        // return the last index
+        return reply;
 
     }
 
@@ -60,11 +77,27 @@ public class Log {
     }
 
     // returns the last log term or -1 if null
-    public long lastLogTerm() {
-        if (this.lastLogIndex()>0) {
-            return ((LogEntry)this.entries.lastElement()).getTerm();
+    public int lastLogTerm() {
+        if (this.lastLogIndex() > 0) {
+            return ((LogEntry) this.entries.lastElement()).getTerm();
         } else return -1;
     }
+
+    // returns term at position index or -1
+    public int getTermAtIndex(int index) {
+        if (this.lastLogIndex() <= index) {
+            LogEntry l = (LogEntry) this.entries.get(index);
+            return l.getTerm();
+        } else return -1;
+    }
+
+    // updates committed index
+    public void updateCommitIndex(int index) {
+        if (index>this.commitIndex) {
+            this.commitIndex=(index<this.lastLogIndex()? index : this.lastLogIndex());
+        }
+    }
+
 
 }
 
@@ -74,14 +107,14 @@ class LogEntry {
     * an entry in the log
      */
 
-    private long term;
+    private int term;
     private String command;
     private Object parameters; // agnostic about
 
-    public LogEntry(long term, String command, Object parameters) {
-        this.term=term;
-        this.command=command;
-        this.parameters=parameters;
+    public LogEntry(int term, String command, Object parameters) {
+        this.term = term;
+        this.command = command;
+        this.parameters = parameters;
     }
 
     public String getCommand() {
@@ -93,7 +126,7 @@ class LogEntry {
     }
 
     public String toString() {
-        return "Term: "+term+" command: "+command;
+        return "Term: " + term + " command: " + command;
     }
 
 }
--- src/com/pvilas/RaftServer.java
+++ src/com/pvilas/RaftServer.java
@@ -19,6 +19,9 @@ public class RaftServer extends CServer {
     public static final int STATE_LEADER = 0;
     public static final int STATE_FOLLOWER = 1;
     public static final int STATE_CANDIDATE = 2;
+    public static final String RPC_VOTE = "RequestVote";
+    public static final String RPC_APPEND = "AppendEntries";
+    public static final String CLIENT_REQUEST = "ClientRequest";
     private int state;
 
     // helper class to make json responses
@@ -28,7 +31,7 @@ public class RaftServer extends CServer {
     private static StateMachine sm = new StateMachine();
 
     // latest term server has seen -- TODO: MUST BE STORED
-    private long currentTerm = 0;
+    private int currentTerm = 0;
     // candidate that received vote in current term -1 if none -- TODO: MUST BE STORED
     private int votedFor = -1;
     // server's log --- TODO: MUST BE STORED
@@ -37,16 +40,71 @@ public class RaftServer extends CServer {
 
     public RaftServer(int number, int port) throws IOException {
         super(number, port); // low-level comm machinery
-        this.state=STATE_FOLLOWER; // server starts as a follower
-        this.log=new Log(number);
+        this.state = STATE_FOLLOWER; // server starts as a follower
+        this.log = new Log(number);
     }
 
 
+    /*
+    *   structure of messages in json format
+     *  "rpc-command", appendEntries or  requestVote
+     *  ============================================
+     *  -> appendEntries
+     *
+     *   request
+     *   -------
+     *          .add("index", 564654)
+     *          .add("term", 1234)
+     *          .add("command", "add" )
+     *          .add("recipe", payload);
+     *    payload =  .add( "Title", "Espárragos a la sevillana" )
+     *          .add( "RecipeNum", 111 )
+     *          .add( "Author", "pvilas")
+     *          .add( "Timestamp", "2013-10-19 15:00:00");
+     *   response
+     *   --------
+     *          .add("term", 564654)
+     *          .add("success", 0)
+     *
+     *  -> requestVote
+     *
+     *          .add("term", 564654)
+     *          .add("candidateId", 1234)
+     *          .add("lastLogIndex", 23 )
+     *          .add("lastLogTerm", 5);
+     *
+     *   response
+     *   --------
+     *          .add("term", 4)
+     *          .add("voteGranted", 1)
+     *
+     *  -- Other: ClientRequest
+     */
     protected void process() {
 
-        // if I'm a follower, I accept a log replication
 
-        // if I'm the leader, I accept a log operation
+        // read message from stream and parse as json object
+        JsonObject message = JsonObject.readFrom( this.read() );
+
+        // extract rpc command
+        String rpcCommand = message.get("rpc-command").toString();
+
+        // I'm a follower, I accept a log replication op
+        if ( state==STATE_FOLLOWER && rpcCommand == RPC_APPEND) {
+
+        }
+
+        // I'm a follower, I accept a client request
+        // TODO: redirect to leader
+        if ( state==STATE_FOLLOWER && rpcCommand == CLIENT_REQUEST) {
+
+        }
+
+        // I'm the leader, I accept a log operation
+        if ( state==STATE_FOLLOWER && rpcCommand == CLIENT_REQUEST) {
+
+        }
+
         // and I will try to replicate it
         // if I will have replicated on the majority of the servers
         // the this entry is commited, I can pass it to the state machine and
@@ -69,17 +127,52 @@ public class RaftServer extends CServer {
     }
 
 
+    // impl of AppendEntries
+    // invoked by leader to replicte log entries 5.3
+    // also used as heartbeat 5.2
+    // @returns true if the entry has appended
+    protected boolean AppendEntries(
+            int termL, // leader's term
+            int leaderId, // so follower can redirect clients
+            int prevLogIndex, // index of log entry immediately preceding new ones
+            int prevLogTerm, // term of prevLogIndex
+            JsonObject entry, // log entries to store (empty for heartbeat)
+            int leaderCommit // leader's commitIndex
+    ) {
+
+        boolean success = false;
+
+        //   1. Reply false if term < currentTerm (§5.1)
+        if (termL < this.currentTerm) return false;
+
+        //    2. Reply false if log doesn’t contain an entry at prevLogIndex
+        //    whose term matches prevLogTerm (§5.3)
+        //    3. If an existing entry conflicts with a new one (same index
+        //    but different terms), delete the existing entry and all that
+        //    follow it (§5.3)
+        //    4. Append any new entries not already in the log
+        success=this.log.append(termL, entry, prevLogTerm, prevLogIndex);
+
+        //    5. If leaderCommit > commitIndex, set commitIndex =
+        //    min(leaderCommit, last log index)
+        this.log.updateCommitIndex(leaderCommit);
+
+        return success;
+    }
+
+
     // impl of requestVote
-    public boolean requestVote( long termC, // candidate's term
-                                int  candidateId, // candidate requesting vote
-                                int  lastLogIndex, // index of candidate's last log entry
-                                long lastLogTerm // term of cadidates's last log entry
+    // @returns vote
+    protected boolean requestVote(int termC, // candidate's term
+                                  int candidateId, // candidate requesting vote
+                                  int lastLogIndex, // index of candidate's last log entry
+                                  int lastLogTerm // term of cadidates's last log entry
     ) {
         // rename parameters to match slide 14
-        long lastTermV=this.log.lastLogTerm();
-        int  lastIndexV=this.log.lastLogIndex();
-        long lastTermC=lastLogTerm;
-        int  lastIndexC=lastLogIndex;
+        int lastTermV = this.log.lastLogTerm();
+        int lastIndexV = this.log.lastLogIndex();
+        int lastTermC = lastLogTerm;
+        int lastIndexC = lastLogIndex;
 
         logger.debug("Being requested to vote");
 
@@ -88,25 +181,25 @@ public class RaftServer extends CServer {
         // this can be also on the logic of the request vote response
         if (this.stepDown(termC)) {
             return false; // vote  NO
-        } else if (termC==this.currentTerm && (this.votedFor == -1 || this.votedFor == candidateId) ) {
+        } else if (termC == this.currentTerm && (this.votedFor == -1 || this.votedFor == candidateId)) {
 
-                // check for log completeness
-                if ( (lastTermV>lastTermC) ||
-                     (lastTermV==lastTermC && lastIndexV>lastIndexC)
+            // check for log completeness
+            if ((lastTermV > lastTermC) ||
+                    (lastTermV == lastTermC && lastIndexV > lastIndexC)
                     ) {
-                    this.votedFor=-1;
-                    return false;
-                } else { // his log is almost complete as mine!
-                    this.votedFor=candidateId;
-                    this.resetElectionTimeout();
-                    return true;
-                }
-
-        }  else if (termC<this.currentTerm) {
-            // this comes form page 4 of the paper
-                this.votedFor=-1;
-                logger.debug("Voted no for server candidate "+candidateId);
+                this.votedFor = -1;
                 return false;
+            } else { // his log is almost complete as mine!
+                this.votedFor = candidateId;
+                this.resetElectionTimeout();
+                return true;
+            }
+
+        } else if (termC < this.currentTerm) {
+            // this comes form page 4 of the paper
+            this.votedFor = -1;
+            logger.debug("Voted no for server candidate " + candidateId);
+            return false;
         }
 
         return false;
@@ -123,13 +216,13 @@ public class RaftServer extends CServer {
     }
 
     // performs an step down
-    private boolean stepDown(long newTerm) {
-        if(newTerm>this.currentTerm) {
+    private boolean stepDown(int newTerm) {
+        if (newTerm > this.currentTerm) {
             if (this.state == this.STATE_LEADER || this.state == this.STATE_CANDIDATE) {
                 this.setState(this.STATE_FOLLOWER);
             }
-            this.currentTerm=newTerm;
-            this.votedFor=-1;
+            this.currentTerm = newTerm;
+            this.votedFor = -1;
         }
         return false;
     }
@@ -137,8 +230,8 @@ public class RaftServer extends CServer {
     // sets server state
     private void setState(int st) {
         // TODO: save on disk before set variable for the case of a crash
-        this.state=st;
-        this.votedFor=-1;
+        this.state = st;
+        this.votedFor = -1;
     }
 
     // return server id
@@ -180,7 +273,12 @@ class JSonHelper {
                 .add("voteGranted", voteGranted);
     }
 
-
+    // return a response to an append op
+    public JsonObject resultAppend(int term, int success) {
+        return new JsonObject()
+                .add("term", term)
+                .add("success", success);
+    }
 
 
 }
